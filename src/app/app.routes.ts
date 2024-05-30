fcaf0f8f99d7e7cf7d7927c5945945c95857bcb7
ERRECHAKI <lamyaaerrechaki@gmail.com>
2024-05-30 09:37:15 +0200

    second commit'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:"blog-details",
+    loadComponent:() => import('./components/blog-details.component')}];
---
+++ src/app/components/blog-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-details.component.html',
+  styleUrl: './blog-details.component.css'
+})
+export default class BlogDetailsComponent {
+
+}
+
+
+
